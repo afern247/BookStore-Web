67a314da1a2cd452efd6863691bd7b678077dabc
Arturo <afern247@protonmail.com>
2019-01-20 15:52:48 -0500

    Users can register now

--- bookStore/users/views.py
+++ bookStore/users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages # to display alert messages when the form data is valid
+from .forms import UserSignUpForm
 
-# Create your views here.
+def signup(request):
+    # If the request is a post, then proceed w/ the form
+    if request.method == 'POST':
+        form = UserSignUpForm(request.POST)
+        # To validate the form
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for { username }.')
+            return redirect ('store-home-page')
+    # if the request is not a post, then show a blank form
+    else:
+        # to use the form at signup we create an instance of the form we imported on top:
+        form = UserSignUpForm()
+    return render(request, 'users/signup.html', {'form': form})
