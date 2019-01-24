9684e769e0918921a3ce7aec4c1528483c0aa584
Arturo <afern247@protonmail.com>
2019-01-24 12:02:03 -0500

    added some comments

--- bookStore/users/models.py
+++ bookStore/users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+# Profle model with data regarding the user
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
@@ -10,8 +11,9 @@ class Profile(models.Model):
     #Image feature upload
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
+    # If we don't have this, it's going to say profile object only
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.user.username} Profile'  # it's going to print username Profile
 
     def save(self, *args, **kwargs):
             super().save(*args, **kwargs)
