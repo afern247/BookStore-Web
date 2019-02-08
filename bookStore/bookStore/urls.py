d0248cbe427b0e21ce2c9ee123018c87d068d9e9
Arturo <afern247@protonmail.com>
2019-02-07 19:21:47 -0500

    test

---
+++ bookStore/bookStore/urls.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from books import views as books_views
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # Administration
+    path('admin/', admin.site.urls),
+
+    # User auth
+    path('signup/', user_views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
+    # Navigation
+    path('', include('storePage.urls')),            # Home page
+    path('settings/', include('users.urls')),       # user profile, billing, etc. settings page
+    path('wishlist/', include('wishlist.urls')),    # Wishlist
+    path('books/', include('books.urls')),          # Books data
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ bookStore/users/views.py
@@ -0,0 +1,102 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages # to display alert messages when the form data is valid
+from .forms import UserSignUpForm, UserUpdateForm, ProfileUpdateForm, UserSignUpForm, UserUpdateForm, ProfileUpdateForm, UserProfileForm, BioAndSocialForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import PasswordChangeForm
+
+
+# User registration page
+def signup(request):
+    # If the request is a post, then proceed w/ the form
+    if request.method == 'POST':
+        form = UserSignUpForm(request.POST)
+        # To validate the form
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for { username }.')
+            return redirect ('store-home-page')
+    # if the request is not a post, then show a blank form
+    else:
+        # to use the form at signup we create an instance of the form we imported on top:
+        form = UserSignUpForm()
+    return render(request, 'users/signup.html', {'form': form})
+
+# When the user clicks on the profile menu, he/she will be redirected here
+def settingsHome(request):
+    return redirect('profile-settings')
+
+# Profile page
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user_ProfileForm = UserProfileForm(request.POST, instance=request.user)
+        user_BioAndSocialForm = BioAndSocialForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_ProfileForm.is_valid() and user_BioAndSocialForm.is_valid():
+            user_ProfileForm.save()
+            user_BioAndSocialForm.save()
+            messages.success(request, f'Your profile has been updated successfully')
+            return HttpResponseRedirect(request.path_info)
+        else:
+            messages.warning(request, f'Please correct the error below.')
+
+    else:
+        user_ProfileForm = UserProfileForm(instance=request.user)
+        user_BioAndSocialForm = BioAndSocialForm(instance=request.user.profile)
+
+    context = {
+        'user_ProfileForm': user_ProfileForm,
+        'user_BioAndSocialForm': user_BioAndSocialForm
+    }
+
+    return render(request, 'users/profile.html', context)
+
+
+# Billing page
+def billingSettings(request):
+    return render(request, 'users/billing.html')
+
+def accountSettings(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, f'Your account has been updated successfully')
+            return HttpResponseRedirect(request.path_info)
+        else:
+            messages.warning(request, f'ERROR! Please read below')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+
+    context = {
+        'u_form': u_form
+    }
+    return render(request, 'users/account.html', context)
+
+
+# User password reset page
+def securitySettings(request):
+    if request.method == 'POST':
+        u_Passform = PasswordChangeForm(request.user, request.POST)
+
+        if u_Passform.is_valid():
+            u_Passform.save()
+            # update_session_auth_hash(request, u_Passform)
+            update_session_auth_hash(request, u_Passform.user)
+            messages.success(request, f'Password has been updated successfully')
+            return HttpResponseRedirect(request.path_info)
+        else:
+            messages.warning(request, f'ERROR!')
+
+    else:
+        u_Passform = PasswordChangeForm(request.user)
+
+    context = {
+        'u_Passform': u_Passform
+    }
+    return render(request, 'users/security.html', context)
