b6b4d437fd36892cbbdc4d7bfc7888af7e7b426a
Tomas Gonzalez <tomasgonz875@gmail.com>
2019-02-12 17:21:46 -0500

    Created the models for Lists/Books needed for the wishlistapp
    Also better organized the wishlist page.

--- bookStore/wishlist/models.py
+++ bookStore/wishlist/models.py
@@ -1,9 +1,21 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
-"""
-class wList(models.Model):
-    book_name = models.CharField(max_length=200)
+
+class List(models.Model):
+    name = models.CharField(max_length=25)
+    date_created = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
     author_name = models.CharField(max_length=50)
-    price = 
-"""
+    price = models.IntegerField()
+    date_added = models.DateTimeField(default=datetime.now, blank=True)
+    list_name = models.ForeignKey(List, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
