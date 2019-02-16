edfc275b2391d39f31d6682222f7b1a04b98ef5c
Arturo <afern247@protonmail.com>
2019-02-15 20:55:24 -0500

    Revert "Able to select main address on profile"
    This reverts commit 2496010a59e8a9cde05e961ef3b779cf3158dcc7.

--- bookStore/users/admin.py
+++ bookStore/users/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Profile, Address, AddressInfo
+from .models import Profile, Address
 
 admin.site.register(Profile)
 admin.site.register(Address)
-admin.site.register(AddressInfo)
