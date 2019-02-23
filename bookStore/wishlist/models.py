eb01799c1342af79a0818b09394e891886ced7f5
Tomas Gonzalez <tomasgonz875@gmail.com>
2019-02-23 17:06:03 -0500

    User field finally done for the List model.
    -Lists can now be attached to a user
    -More testing needs to be done once other users can be created.

--- bookStore/wishlist/models.py
+++ bookStore/wishlist/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 from datetime import datetime
-from django.contrib.auth.models import User
-from django.conf import settings
+from users.models import Profile
 
 class List(models.Model):
-    #user = models.ForeignKey(User, default=0, on_delete=models.CASCADE)
     name = models.CharField(max_length=25)
     date_created = models.DateTimeField(default=datetime.now, blank=True)
-    user_name = models.CharField(max_length=150, default="Testing")
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -15,7 +13,7 @@ class List(models.Model):
 class Book(models.Model):
     name = models.CharField(max_length=200)
     author_name = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=4, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     date_added = models.DateTimeField(default=datetime.now, blank=True)
     list_name = models.ForeignKey(List, on_delete=models.CASCADE)
 
