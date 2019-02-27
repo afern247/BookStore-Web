89022f59b4cacd146682c4e8667af6cbbe515f9a
Arturo <afern247@protonmail.com>
2019-02-27 12:30:15 -0500

    Added nickname

--- bookStore/users/models.py
+++ bookStore/users/models.py
@@ -4,13 +4,13 @@ from PIL import Image
 
 
 class Address(models.Model):
-    user = models.ForeignKey('Profile', related_name='address', on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
     city = models.CharField(max_length=60, default="Miami")
     state = models.CharField(max_length=30, default="Florida")
     zipcode = models.CharField(max_length=5, default="33165")
     country = models.CharField(max_length=50)
+    user = models.ForeignKey('Profile', related_name='addresses', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = 'Address'
@@ -49,6 +49,7 @@ class Address(models.Model):
 # All user data is/should be linked to this profile, so when user gets deleted, all data deletes as well
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nick_name = models.CharField('Nick name', max_length=30, blank=True, default='')
     bio = models.TextField(max_length=500, blank=True)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     # Address = models.ForeignKey('Address', on_delete=models.CASCADE)
