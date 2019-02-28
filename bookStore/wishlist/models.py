5207dec62bd7ceee2ae0b75cf89baf44181d08f4
Tomas Gonzalez <tomasgonz875@gmail.com>
2019-02-28 02:33:51 -0500

    Lists now can be attached to books.
    -Updated the List model so it can now be attached to books.
    -Still need to add the ability to display the content within the lists for the user.
    -Also need to divide up the page to show the content in an organized way separated by list.

--- bookStore/wishlist/models.py
+++ bookStore/wishlist/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from datetime import datetime
 from users.models import Profile
+from bookDetails.models import Book
 
 class List(models.Model):
     name = models.CharField(max_length=25)
     date_created = models.DateTimeField(default=datetime.now, blank=True)
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    list_num = models.IntegerField(range(1, 3))
+    books = models.ManyToManyField(Book, blank=True)
 
     def __str__(self):
         return self.name
 
+"""
 class Book(models.Model):
     name = models.CharField(max_length=200)
     author_name = models.CharField(max_length=50)
@@ -20,3 +22,4 @@ class Book(models.Model):
 
     def __str__(self):
         return self.name
+"""
