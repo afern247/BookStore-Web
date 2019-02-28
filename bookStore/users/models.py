11dbeec8fe14b0629fd1bd437eaa3a1fa580952f
Arturo <afern247@protonmail.com>
2019-02-28 11:13:04 -0500

    Revert "Merge branch 'Misc' into Development"
    This reverts commit 950e6c40aef13bf756ec18b5b55d4eda5c51a43e.

--- bookStore/users/models.py
+++ bookStore/users/models.py
@@ -4,6 +4,7 @@ from PIL import Image
 
 
 class Address(models.Model):
+    user = models.ForeignKey('Profile', related_name='address', on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
     city = models.CharField(max_length=60, default="Miami")
@@ -12,23 +13,45 @@ class Address(models.Model):
     country = models.CharField(max_length=50)
 
     class Meta:
-        verbose_name = 'Address'
         verbose_name_plural = 'Address'
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
+
+
+
+
+
+
+# class AddressInfo(models.Model):
+
+#     HOME_ADDRESS = 1
+#     SHIPPING_ADDRESS = 2
+
+#     TYPE_ADDRESS_CHOICES = (
+#         (HOME_ADDRESS, "Home address"),
+#         (SHIPPING_ADDRESS, "Shipping address"),
+#     )
+
+#     address = models.ForeignKey('Address', on_delete=models.CASCADE)
+#     profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+
+#     # This is the field you would use for know the type of address.
+#     address_type = models.PositiveIntegerField(choices=TYPE_ADDRESS_CHOICES)
+
+#     class Meta:
+#         verbose_name_plural = 'AddressInfo'
+#         verbose_name = 'Addresses of all users'
+
+
+
 
 # All user data is/should be linked to this profile, so when user gets deleted, all data deletes as well
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nick_name = models.CharField('Nick name', max_length=30, blank=True, default='')
     bio = models.TextField(max_length=500, blank=True)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    addresses = models.ManyToManyField(
-        Address,
-        through='AddressType',
-        through_fields=('address', 'profile')
-        )
+    # Address = models.ForeignKey('Address', on_delete=models.CASCADE)
 
     # If we don't have this, it's going to say profile object only
     def __str__(self):
@@ -44,26 +67,3 @@ class Profile(models.Model):
                 img.thumbnail(output_size)
                 img.save(self.image.path)
 
-
-class AddressType(models.Model):
-
-    HOME_ADDRESS = 1
-    SHIPPING_ADDRESS = 2
-
-    TYPE_ADDRESS_CHOICES = (
-        (HOME_ADDRESS, "Home address"),
-        (SHIPPING_ADDRESS, "Shipping address"),
-    )
-
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-
-    # This is the field you would use for know the type of address.
-    address_type = models.PositiveIntegerField(choices=TYPE_ADDRESS_CHOICES)
-
-    class Meta:
-        verbose_name = 'Type of Address'
-        verbose_name_plural = 'Type of Address'
-
-    def __str__(self):
-        return f'{self.address_type}'
