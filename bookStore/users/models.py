e339d439dc82e28a4b940705a42285366879e980
Arturo <afern247@protonmail.com>
2019-02-28 12:12:31 -0500

    Revert "Merge branch 'Misc' into Development"
    This reverts commit 24c7953562276e9870c4d2e1c4d57416c6622aac.

--- bookStore/users/models.py
+++ bookStore/users/models.py
@@ -4,7 +4,6 @@ from PIL import Image
 
 
 class Address(models.Model):
-    user = models.ForeignKey('Profile', related_name='address', on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
     city = models.CharField(max_length=60, default="Miami")
@@ -19,22 +18,17 @@ class Address(models.Model):
     def __str__(self):
         return self.name
 
-
-
-
 # All user data is/should be linked to this profile, so when user gets deleted, all data deletes as well
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nick_name = models.CharField('Nick name', max_length=30, blank=True, default='')
     bio = models.TextField(max_length=500, blank=True)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    # Address = models.ForeignKey('Address', on_delete=models.CASCADE)
 
 
     # If we don't have this, it's going to say profile object only
     def __str__(self):
-        return self.user.username
-        #return f'{self.user.username}'  # it's going to print username Profile
+        return f'{self.user.username} Profile'  # it's going to print username Profile
 
     def save(self, *args, **kwargs):
             super().save(*args, **kwargs)
