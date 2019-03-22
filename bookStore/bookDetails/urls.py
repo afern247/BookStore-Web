00f58df79019356c88500b34e8fac92af5d31839
pfran040 <42625194+pfran040@users.noreply.github.com>
2019-03-22 02:39:25 -0400

    Update urls.py

--- bookStore/bookDetails/urls.py
+++ bookStore/bookDetails/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path('', views.book_list, name='book_list'),
     path('<slug:author_slug>/', views.book_list, name='book_list_by_author'),
     path('<str:book_name>/<slug:slug>/', views.book_info, name='book_info'),
-    path('<str:book_name>/<slug:slug>/comment/', views.add_comment, name='add_comment'),
+    path('<str:book_name>/<slug:slug>/review/', views.add_review, name='add_review'),
 ]
