048f2e4d85f774ec3c8a988ea6c5b7fdc2d0babe
Tomas Gonzalez <tomasgonz875@gmail.com>
2019-03-22 00:47:49 -0400

    Added some comments to wishlist/urls.py

--- bookStore/wishlist/urls.py
+++ bookStore/wishlist/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('deleteList/<int:list_id>', views.deleteList, name="deleteList"),
     path('rename/<int:list_id>', views.rename, name="rename"),  #NOT IMPLEMENTED YET
     path('deleteBook/<int:list_id>/<int:book_id>', views.deleteBook, name="deleteBook"),
-    path('moveToCart/<int:list_id>/<int:book_id>', views.moveToCart, name="moveToCart"),
-    path('moveBook/<int:listFrom_id>/<int:listTo_id>/<int:book_id>', views.moveBook, name="moveBook"),  #NOT IMPLEMENTED YET
+    path('moveToCart/<int:list_id>/<int:book_id>', views.moveToCart, name="moveToCart"),    #CAN'T ADD TO CART
+    path('moveBook/<int:listFrom_id>/<int:listTo_id>/<int:book_id>', views.moveBook, name="moveBook"),
 ]
