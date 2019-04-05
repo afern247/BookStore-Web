bad9dc5b100124b127817fc0ceabbcc2302a3c05
pfran040 <42625194+pfran040@users.noreply.github.com>
2019-04-05 17:40:50 -0400

    add_review view updated

--- bookStore/bookDetails/views.py
+++ bookStore/bookDetails/views.py
@@ -64,14 +64,32 @@ def book_info(request, book_name, slug):
                                                             'myLists': myLists})
 
 def add_review(request, book_name, slug):
-    book = get_object_or_404(Book, book_name=book_name, slug=slug)
+    book = get_object_or_404(Book, book_name=book_name, slug=slug) #Obtain book info
+    User = get_object_or_404(Profile, user=request.user)           #Obtain user info for comment
+
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
-            comment = form.save(commit=False)
-            comment.user = request.user
-            comment.book = book
-            comment.save()
+            message     = form.cleaned_data['message']
+            name        = request.POST.get('name_select')       #Get value of name_select from radio buttons
+            rating      = request.POST.get('rating')            #Get rating from stars
+            review      = form.save(commit=False)
+            review.book = book                                  #Get book for which this review is being applied to
+            if name == "Username":                              #Check what user selected from name_select radio buttons
+                review.name = User.user
+            elif name == "Nickname":
+                review.name = User.nick_name
+                if len(review.name) == 0:                       #If nickname is not filled, use username
+                    review.name = User.user
+            elif name == "Anonymous":
+                review.name = "Anonymous"
+            else:
+                review.name = "Anonymous"
+            review.rating = rating
+            #print("Username Display: " + review.user)          #Testing what username and rating are
+            print("Rating submitted: " + review.rating)
+            review.save()                                       #Save form and then redirect back to book_info page for
+                                                                #specific book
             return redirect('bookDetails:book_info', book_name=book.book_name, slug=book.slug)
     else:
         form = ReviewForm()
