c452d54f228e48897d93d36db964bae0ea336e4c
Tomas Gonzalez <tomasgonz875@gmail.com>
2019-04-14 14:09:33 -0400

    Deleted some extra code to make it cleaner.

--- bookStore/cart/views.py
+++ bookStore/cart/views.py
@@ -47,7 +47,6 @@ def addToCart(request, book_id):
     # Code for Wish List since I needed to send the data as GET
     elif request.method == 'GET':
         userCart.add(book=book, amount=1, change_amount=False)
-        return redirect('cart:cart_info')
 
     # Once finished, the function redirects the user to the page
     # that shows them the contents of their cart
